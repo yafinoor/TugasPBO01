a7008b991b1ef4bb0f8adfba4779216477326f34
yafinoor <mnyafi777@gmail.com>
2018-03-26 23:27:49 +0800

    memanggil method elang dan angsa

--- src/tugaspbo01/UnggasAksi.java
+++ src/tugaspbo01/UnggasAksi.java
@@ -12,14 +12,29 @@ package tugaspbo01;
 public class UnggasAksi {
     public static void main(String[] args) {
         unggas Ayam = new unggas();
+            Ayam.bentuk_cakar="cakar kecil";
+                Ayam.bentuk_paruh="runcing kecil";
+                    Ayam.panjang_bulu="2 cm";
+                Ayam.ukuran_tubuh="kecil";
+            Ayam.warna_bulu="hitam merah";
         
-        Ayam.bentuk_cakar="cakar kecil";
-        Ayam.bentuk_paruh="runcing kecil";
-        Ayam.panjang_bulu="2 cm";
-        Ayam.ukuran_tubuh="kecil";
-        Ayam.warna_bulu="hitam merah";
-        
-         Ayam.cetakinfo();
+        Ayam.cetakinfo();
+   
+        unggas Elang = new unggas();      
+            Elang.bentuk_cakar="cakar kecil";
+                Elang.bentuk_paruh="runcing kecil";
+                    Elang.panjang_bulu="2 cm";
+                Elang.ukuran_tubuh="kecil";
+            Elang.warna_bulu="hitam merah";
+  
+         Elang.cetakinfo();
+         
+        unggas Angsa = new unggas();
+            Angsa.bentuk_cakar="cakar kecil";
+                Angsa.bentuk_paruh="runcing kecil";
+                    Angsa.panjang_bulu="2 cm";
+                Angsa.ukuran_tubuh="kecil";
+            Angsa.warna_bulu="hitam merah";
+        Angsa.cetakinfo();
     }
-    
 }
