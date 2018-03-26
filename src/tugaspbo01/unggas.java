fb970283be33070d9621ae761c39e40545dae300
yafinoor <mnyafi777@gmail.com>
2018-03-26 23:07:03 +0800

    membuat atribut dan behavior

--- src/tugaspbo01/unggas.java
+++ src/tugaspbo01/unggas.java
@@ -10,5 +10,18 @@ package tugaspbo01;
  * @author Asus
  */
 public class unggas {
+    String panjang_bulu;
+    String warna_bulu;
+    String ukuran_tubuh;
+    String bentuk_cakar;
+    String bentuk_paruh;
+    
+    void cetakinfo(){
+        System.out.println("Panjang Bulu    :"+panjang_bulu);
+        System.out.println("Warna Bulu      :"+warna_bulu);
+        System.out.println("ukuran tubuh    :"+ukuran_tubuh);
+        System.out.println("bentuk cakar    :"+bentuk_cakar);
+        System.out.println("bentuk paruh    :"+bentuk_paruh);
+    }
     
 }
